b119f33823056c794676e9ed4e94b69d6240ecd8
Robin De Schepper <robin.deschepper93@gmail.com>
2020-01-28 23:11:20 +0100

    Added `synapse.stimulate`

--- arborize/synapse.py
+++ arborize/synapse.py
@@ -8,8 +8,12 @@ class Synapse:
         self._point_process_name = point_process_name
         self._point_process_glia_name = g.resolve(point_process_name, pkg="dbbs_mod_collection", variant=variant)
         self._point_process = g.insert(section, point_process_name, pkg="dbbs_mod_collection", variant=variant)
+        section.__ref__(self)
         for key, value in attributes.items():
             setattr(self._point_process, key, value)
 
     def __neuron__(self):
         return self._point_process.__neuron__()
+
+    def stimulate(self, *args, **kwargs):
+        return self._point_process.stimulate(*args, **kwargs)
