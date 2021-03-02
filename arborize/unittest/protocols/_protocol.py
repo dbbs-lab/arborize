5136125759699256b3cdd2b0171f411da3d893b7
Robin De Schepper <robin.deschepper93@gmail.com>
2021-03-02 16:02:02 +0100

    Factored out protocol assertions under abstract `asserts` method

--- arborize/unittest/protocols/_protocol.py
+++ arborize/unittest/protocols/_protocol.py
@@ -1,12 +1,15 @@
 class TestProtocol:
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
-        abstracts = ["prepare", "run", "results"]
-        missing = [a for a in abstracts if a not in cls.__dict__]
+        abstracts = ["prepare", "run", "results", "asserts"]
+        missing = [a for a in abstracts if not hasattr(cls, a)]
         if missing:
-            raise RuntimeError(f"Can't instantiate `{cls.__name__}` with abstract methods " + ",".join(f"`{m}`" for m in missing))
+            raise RuntimeError(f"Can't instantiate protocol `{cls.__name__}` with abstract methods " + ",".join(f"`{m}`" for m in missing))
 
-    def __call__(self, test):
+    def __call__(self, test, asserts=True):
         self.prepare(test.setup)
         self.run()
-        return self.results(test)
+        results = self.results(test.setup)
+        if asserts:
+            self.asserts(test, results)
+        return results
