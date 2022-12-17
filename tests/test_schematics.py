5a08a1de1fbb1e3913d919fc6ba99ca977350927
Robin De Schepper <robin.deschepper93@gmail.com>
2022-12-17 14:07:08 +0100

    added weak testing of file driver

--- tests/test_schematics.py
+++ tests/test_schematics.py
@@ -3,7 +3,19 @@ from arborize import file_schematic
 
 
 class TestFileSchematic(unittest.TestCase):
-    def test_path(self):
-        print("ELLLLO")
-        with open("tests/morphologies/cell010.swc", "r") as f:
+    def test_labels(self):
+        with open("tests/morphologies/P75.swc", "r") as f:
             schematic = file_schematic(f)
+        self.assertEqual(
+            sorted(["soma", "apical_dendrite", "basal_dendrite"]),
+            sorted(set(b.labels[0] for b in schematic)),
+            "Missing or added label types",
+        )
+
+    def test_tags(self):
+        schematic = file_schematic("tests/morphologies/cell010.swc")
+        self.assertEqual(
+            sorted(["soma", "tag_7"]),
+            sorted(set(b.labels[0] for b in schematic)),
+            "Missing or added label types",
+        )
