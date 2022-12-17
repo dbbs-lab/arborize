d7be28d91e04c2e875414074e72b09bed880f8a5
Robin De Schepper <robin.deschepper93@gmail.com>
2022-12-17 14:04:29 +0100

    file driver

--- arborize/schematics/_file.py
+++ arborize/schematics/_file.py
@@ -1,73 +1,90 @@
 import os
+import re
 import tempfile
 import itertools
+import warnings
 from typing import Optional, TYPE_CHECKING, Union, TextIO
-from ..schematic import Schematic
 
+from morphio import SomaType
+
+from ..schematic import Schematic
 
 if TYPE_CHECKING:
     from ..schematic import ModelDefinition
 
 
 def file_schematic(
-    pathOrFileLike: Union["os.PathLike", TextIO],
+    file_like: Union["str", "os.PathLike", TextIO],
     definitions: Optional["ModelDefinition"] = None,
     fname: str = None,
 ) -> Schematic:
-    if hasattr(pathOrFileLike, "read"):
-        if not pathOrFileLike.name and not fname:
+    if hasattr(file_like, "read"):
+        if not file_like.name and not fname:
             raise IOError(
                 "The file-driver MorphIO requires a file name to parse files. "
                 "Use a file-like object that provides a `name` attribute, "
                 "or pass the `fname` keyword argument, "
                 "with a suffix matching the file format."
             )
-        base = os.path.basename(pathOrFileLike.name or fname)
+        base = os.path.basename(file_like.name or fname)
         handle, abspath = tempfile.mkstemp(suffix=base)
         os.close(handle)
         try:
             with open(abspath, "w") as f:
-                f.write(pathOrFileLike.read())
+                f.write(file_like.read())
             return file_schematic(abspath, definitions)
         finally:
             os.unlink(abspath)
     import morphio
 
-    morpho = morphio.Morphology(os.fspath(pathOrFileLike))
+    morpho = morphio.Morphology(os.fspath(file_like))
     schematic = Schematic()
-    print(morpho.soma)
-    print(morpho.root_sections)
+    print(morpho.soma, dir(morpho.soma))
+    print(morpho.root_sections, dir(morpho.root_sections[0]))
     print(
         len([*morpho.root_sections[0].iter()]),
         sum(len([*i.iter()]) for i in morpho.root_sections),
     )
     morpho.root_sections
-    # branches: list["Branch"] = [*morphology.branches]
-    # endpoints = []
-    # for bid, branch in enumerate(branches):
-    #     branch._tempid = bid
-    #     if not len(branch):
-    #         true_parent = None
-    #         while True:
-    #             parent = branch.parent
-    #             if parent is None:
-    #                 break
-    #             elif len(parent):
-    #                 true_parent = endpoints[parent._tempid]
-    #                 break
-    #         schematic.create_empty()
-    #         endpoints.append(true_parent)
-    #     else:
-    #         if branch.parent is not None:
-    #             endpoint = endpoints[branch.parent._tempid]
-    #         else:
-    #             endpoint = None
-    #         for pid, coords, radius, labels in zip(
-    #             itertools.count(), branch.points, branch.radii, branch.labels.walk()
-    #         ):
-    #             endpoint = endpoint if pid == 0 else None
-    #             schematic.create_location((bid, pid), coords, radius, labels, endpoint)
-    #         endpoints.append((bid, pid))
+    branches = [
+        morpho.soma,
+        *itertools.chain.from_iterable(s.iter() for s in morpho.root_sections),
+    ]
+    endpoints = []
+    for bid, branch in enumerate(branches):
+        mid = getattr(branch, "id", -1) + 1
+        if bid != mid:
+            raise AssertionError("MorphIO deviated from depth-first order.")
+        parent = None if getattr(branch, "is_root", True) else branch.parent
+        if not len(branch.points):
+            true_parent = None
+            while True:
+                if parent is None:
+                    break
+                elif len(parent.points):
+                    true_parent = endpoints[parent.id + 1]
+                    break
+                parent = None if branch.is_root else branch.parent
+            schematic.create_empty()
+            endpoints.append(true_parent)
+        else:
+            if parent is not None:
+                endpoint = endpoints[parent.id + 1]
+            else:
+                endpoint = None
+            if isinstance(branch.type, SomaType):
+                type = "soma"
+            elif "custom" in str(branch.type):
+                num = re.search(r"\d+$", str(branch.type)).group()
+                type = f"tag_{num}"
+            else:
+                type = str(branch.type).split(".")[-1]
+            for pid, coords, diam in zip(
+                itertools.count(), branch.points, branch.diameters
+            ):
+                endpoint = endpoint if pid == 0 else None
+                schematic.create_location((bid, pid), coords, diam / 2, [type], endpoint)
+            endpoints.append((bid, pid))
     if definitions is not None:
         schematic.definition = definitions
     return schematic
