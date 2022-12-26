774f7c41d70662a7c4fcb4a8dc0d24600df7b725
Robin De Schepper <robin.deschepper93@gmail.com>
2022-12-26 18:08:09 +0100

    Added more error categories

--- arborize/exceptions.py
+++ arborize/exceptions.py
@@ -4,13 +4,21 @@ from typing import Type
 _make_tree(
     globals(),
     ArborizeError=_e(
-        ModelDefinitionError=_e(),
-        SchematicError=_e(ConstructionError=_e(), FrozenError=_e()),
+        ModelDefinitionError=_e("definition"),
+        ModelError=_e(
+            "model",
+            UnknownLocationError=_e("location"),
+            UnknownSynapseError=_e("synapse"),
+        ),
+        SchematicError=_e("schematic", ConstructionError=_e(), FrozenError=_e()),
     ),
 )
 
 ArborizeError: Type[Exception]
 ModelDefinitionError: Type[ArborizeError]
+ModelError: Type[ArborizeError]
+UnknownLocationError: Type[ModelError]
+UnknownSynapseError: Type[ModelError]
 SchematicError: Type[ArborizeError]
 ConstructionError: Type[SchematicError]
 FrozenError: Type[SchematicError]
