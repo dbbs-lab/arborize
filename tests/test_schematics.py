93b77ac3cf895f507d3e590baef606d06634353c
Robin De Schepper <robin.deschepper93@gmail.com>
2023-10-18 14:21:22 +0200

    add new tests

--- tests/test_schematics.py
+++ tests/test_schematics.py
@@ -1,5 +1,10 @@
 import unittest
+
+import numpy as np
+
+from arborize import define_model
 from tests._shared import SchematicsFixture
+from arborize.schematics import file_schematic
 
 
 class TestFileSchematic(SchematicsFixture, unittest.TestCase):
@@ -21,3 +26,47 @@ class TestFileSchematic(SchematicsFixture, unittest.TestCase):
         # FIXME: When https://github.com/BlueBrain/MorphIO/issues/469 is fixed we can use
         # MorphIO to load SWC files that have multiple different tags on a branch
         pass
+
+    def test_one_branch(self):
+        # Expect soma + one branch
+        self.assertEqual(2, len(self.one_branch.cables), "expected 2 branches")
+        self.assertEqual(
+            2, len(self.one_branch.cables[0].points), "expected 2 soma points"
+        )
+        # Expect the unit branch to be labelled as "soma"
+        self.assertEqual(
+            ["soma"],
+            self.one_branch.cables[0].points[0].branch.labels,
+            "expected soma labels on branch 0",
+        )
+        # Expect 2 + 0 inferred points on the next branch (no point inference on soma)
+        self.assertEqual(
+            2, len(self.one_branch.cables[1].points), "expected 2 branch points"
+        )
+
+    def test_two_branch(self):
+        # Expect soma + two branches
+        self.assertEqual(3, len(self.two_branch.cables), "expected 3 branches")
+        self.assertEqual(
+            2, len(self.two_branch.cables[0].points), "expected 2 soma points"
+        )
+        # Expect the unit branch to be labelled as "soma"
+        self.assertEqual(
+            ["soma"],
+            self.two_branch.cables[0].points[0].branch.labels,
+            "expected soma labels on branch 0",
+        )
+        # Expect 2 + 0 inferred points on the next branch (no point inference on soma)
+        self.assertAlmostEqual(
+            2, len(self.two_branch.cables[1].points), 5, "expected 2 branch points"
+        )
+        self.assertAlmostEqual(
+            0.3,
+            self.two_branch.cables[1].points[0].radius,
+            5,
+            "incorrect radius",
+        )
+        self.assertTrue(
+            np.array_equal([0.0, 6.0, 0.0], self.two_branch.cables[1].points[0].coords),
+            "incorrect coords",
+        )
