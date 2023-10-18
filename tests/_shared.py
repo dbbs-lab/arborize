c9aa28f5a40d999dc229315a06fcd1b1f706cd82
Robin De Schepper <robin.deschepper93@gmail.com>
2023-10-18 14:21:09 +0200

    refactor test schemas to be lazy loaded when the test accesses them

--- tests/_shared.py
+++ tests/_shared.py
@@ -1,4 +1,5 @@
 import typing
+from .data import schematics
 
 if typing.TYPE_CHECKING:
     from arborize.schematic import Schematic
@@ -8,14 +9,26 @@ class SchematicsFixture:
     p75: "Schematic"
     cell010: "Schematic"
     p75_pas: "Schematic"
+    one_branch: "Schematic"
+    two_branch: "Schematic"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def setUp(self) -> None:
-        super().setUp()
-        from .data import schematics
-
+    def tearDown(self):
+        super().tearDown()
         for k, v in vars(schematics).items():
             if not k.startswith("_"):
-                setattr(self, k, v)
+                try:
+                    delattr(self, k)
+                except Exception:
+                    pass
+
+    def __getattr__(self, attr):
+        schemas = vars(schematics)
+        if attr in schemas:
+            schema = schemas[attr]()
+            setattr(self, attr, schema)
+            return schema
+        else:
+            return self.__getattribute__(attr)
--- tests/data/schematics.py
+++ tests/data/schematics.py
@@ -8,10 +8,32 @@ def _mpath(path):
     return _os.path.join(_os.path.dirname(__file__), "morphologies", path)
 
 
-cell010 = _schema(_mpath("cell010.swc"))
-with open(_mpath("P75.swc"), "r") as _file:
-    p75 = _schema(_file)
-with open(_mpath("P75.swc"), "r") as _file:
-    p75_pas = _schema(_file, definitions=_pas)
-    p75_expsyn = _schema(_file, definitions=_expsyn)
-multitagged = _schema(_mpath("multitagged.swc"))
+def cell010():
+    return _schema(_mpath("cell010.swc"))
+
+
+def p75():
+    with open(_mpath("P75.swc"), "r") as _file:
+        return _schema(_file)
+
+
+def p75_pas():
+    with open(_mpath("P75.swc"), "r") as _file:
+        return _schema(_file, definitions=_pas)
+
+
+def p75_expsyn():
+    with open(_mpath("P75.swc"), "r") as _file:
+        return _schema(_file, definitions=_expsyn)
+
+
+def multitagged():
+    return _schema(_mpath("multitagged.swc"))
+
+
+def one_branch():
+    return _schema(_mpath("one_branch.swc"))
+
+
+def two_branch():
+    return _schema(_mpath("two_branch.swc"))
