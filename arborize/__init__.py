e068cbf4aef54f5788da7637a474f25e80fc8e68
Robin De Schepper <robin.deschepper93@gmail.com>
2024-02-13 16:48:51 +0100

    Bump version: 4.0.0b3 → 4.0.0b4

--- arborize/__init__.py
+++ arborize/__init__.py
@@ -16,7 +16,7 @@ from .definitions import (
 from .schematic import Schematic
 from .schematics import bsb_schematic, file_schematic
 
-__version__ = "4.0.0b3"
+__version__ = "4.0.0b4"
 __all__ = [
     "CableProperties",
     "CableType",
