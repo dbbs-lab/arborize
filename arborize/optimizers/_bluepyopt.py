43f6f5f5e3eee347a47606c75600289b47f6ea3c
Robin De Schepper <robin.deschepper93@gmail.com>
2024-02-16 18:19:52 +0100

    wip struggling with cable types, is different when using compounds

--- arborize/optimizers/_bluepyopt.py
+++ arborize/optimizers/_bluepyopt.py
@@ -1,4 +1,4 @@
-import time
+import re
 import typing
 from collections import defaultdict
 
@@ -13,30 +13,36 @@ if typing.TYPE_CHECKING:
 
 
 class ArborizeMorphology(ephys.morphologies.Morphology):
-    def __init__(self, schematic: "Schematic"):
+    def __init__(self, schematic: "Schematic", debug=None):
         super().__init__()
         self._schematic = schematic
+        self._debug = debug
 
     def instantiate(self, sim, icell):
         arborized_cell = neuron_build(self._schematic)
+        sections = set()
         for label in self._schematic.get_cable_types().keys():
             section_list = getattr(icell, label)
-            for sec in arborized_cell.filter_sections([label]):
+            labels = [label]
+            # labels = [l.replace("__", "_") for l in re.split("(?<!_)_(?!_)", label)]
+            for sec in arborized_cell.filter_sections(labels):
+                sections.add(sec.hname())
                 section_list.append(sec.__neuron__())
+        print("Added", len(sections), "sections")
 
     def destroy(self, sim=None):
         pass
 
 
-def get_bpo_cell(schematic, name_cell="cell"):
-    morph = ArborizeMorphology(schematic)
+def get_bpo_cell(schematic, name_cell="cell", debug=None):
+    morph = ArborizeMorphology(schematic, debug=debug)
     schematic.freeze()
     constraints = schematic.definition
     if not isinstance(constraints, ConstraintsDefinition):
         raise TypeError(
             f"Optimization schematic must contain constraints, got {type(constraints)} instead."
         )
-    cable_types = constraints.get_cable_types()
+    cable_types = schematic.get_cable_types()
 
     bpyopt_seclists = {
         label: ephys.locations.NrnSeclistLocation(label, seclist_name=label)
@@ -55,17 +61,48 @@ def get_bpo_cell(schematic, name_cell="cell"):
         for mech, locations in mech_locations.items()
     ]
 
-    bpyopt_params = [
+    bpyopt_cable_params = [
+        ephys.parameters.NrnSectionParameter(
+            name=f"{label}_{prop}",
+            param_name=prop,
+            locations=[bpyopt_seclists[label]],
+            **_to_bpyopt_kwargs(constraint, f"{label}_{prop}"),
+        )
+        for label, cable_type in cable_types.items()
+        for prop, constraint in cable_type.cable
+    ]
+
+    bpyopt_mech_params = [
         ephys.parameters.NrnSectionParameter(
             name=f"{param}_{glia.resolve(mech_id)}_{label}",
             param_name=f"{param}_{glia.resolve(mech_id)}",
             locations=[bpyopt_seclists[label]],
-            **_to_bpyopt_kwargs(constraint),
+            **_to_bpyopt_kwargs(constraint, ""),
         )
         for label, cable_type in cable_types.items()
         for mech_id, mech in cable_type.mechs.items()
         for param, constraint in mech.parameters.items()
     ]
+
+    bpyopt_params = [
+        ephys.parameters.NrnGlobalParameter(
+            "temperature", param_name="celsius", value=32, frozen=True
+        ),
+        *bpyopt_mech_params,
+        *bpyopt_cable_params,
+    ]
+
+    return ephys.models.CellModel(
+        name_cell,
+        morph,
+        bpyopt_mechs,
+        bpyopt_params,
+        secarray_names=[],
+        seclist_names=[*bpyopt_seclists.keys()],
+    )
+
+
+def load_mechs_params(constraints: "ConstraintsDefinition"):
 
     # # todo: params: cable [frozen=true]
     #
@@ -84,9 +121,12 @@ def get_bpo_cell(schematic, name_cell="cell"):
     return bpyopt_mechs, bpyopt_params, [*bpyopt_seclists.keys()]
 
 
-def _to_bpyopt_kwargs(constraint: "ConstraintValue"):
-    constraint = Constraint.from_value(constraint)
+def _to_bpyopt_kwargs(constraint: "Constraint", label: str):
     frozen = constraint.upper == constraint.lower
+    if label == "axon_Ra":
+        constraint.upper = 100
+    elif label == "axon_cm":
+        constraint.upper = 1
     return dict(
         frozen=frozen,
         bounds=[constraint.lower, constraint.upper] if not frozen else None,
--- temp.py
+++ temp.py
@@ -1,37 +1,155 @@
 import bluepyopt as bpop
 import bluepyopt.ephys as ephys
 import dbbs_models
-import numpy as np
 from bsb.morphologies import parse_morphology_file
 
-from arborize import define_constraints, define_model
-from arborize.builders import neuron_build
+from arborize import define_constraints
 from arborize.optimizers._bluepyopt import get_bpo_cell
 from arborize.schematics import bsb_schematic
 
-morpho = parse_morphology_file(
-    "../single_cell_test/dbbs_model_test/Granule_cell/morphology/GranuleCell.swc",
-    tags=dbbs_models.GranuleCellModel.swc_tags,
-)
 constraints = define_constraints(
     {
+        "synapse_types": {
+            ("AMPA", "granule"): {
+                "tau_facil": 5,
+                "tau_rec": 8,
+                "tau_1": 1,
+                "gmax": 140000,
+                "U": 0.43,
+            },
+            ("NMDA", "granule"): {
+                "tau_facil": 5,
+                "tau_rec": 8,
+                "tau_1": 1,
+                "gmax": 23500,
+                "U": 0.43,
+            },
+            ("GABA", "granule"): {"U": 0.35},
+        },
         "cable_types": {
             "soma": {
+                "cable": {"Ra": 100, "cm": 2},
+                "ions": {"k": {"rev_pot": -80.993}, "ca": {"rev_pot": 137.5}},
                 "mechanisms": {
-                    "Leak": {"e": -60, "gmax": [0.00029038073716, 0.00029038073717]}
+                    "Leak": {"e": -60, "gmax": [0.00029038073716, 0.00029038073717]},
+                    "Kv3_4": {"gkbar": 0.00076192450952},
+                    "Kv4_3": {"gkbar": 0.00281496839061},
+                    "Kir2_3": {"gkbar": 0.00074725514702},
+                    "Ca": {"gcabar": 0.00060938071784},
+                    "Kv1_1": {"gbar": 0.00569738264555},
+                    "Kv1_5": {"gKur": 0.00083407556714},
+                    "Kv2_2": {"gKv2_2bar": 1.203410852e-05},
+                    ("cdp5", "CR"): {},
                 },
             },
-            "dendrites": {},
-            "axon": {},
-            "ascending_axon": {},
-            "parallel_fiber": {},
-            "axon_initial_segment": {},
-            "axon_hillock": {},
+            "dendrites": {
+                "cable": {"Ra": 100, "cm": 2.5},
+                "ions": {"k": {"rev_pot": -80.993}, "ca": {"rev_pot": 137.5}},
+                "mechanisms": {
+                    "Leak": {"e": -60, "gmax": 0.00025029700737},
+                    ("Leak", "GABA"): {},
+                    "Ca": {"gcabar": 0.00500128008459},
+                    "Kca1_1": {"gbar": 0.01001807454651},
+                    "Kv1_1": {"gbar": 0.00381819207934},
+                    ("cdp5", "CR"): {},
+                },
+            },
+            "axon": {"cable": {}, "ions": {}, "mechanisms": {}},
+            "ascending_axon": {
+                "cable": {"Ra": 100, "cm": 1},
+                "ions": {
+                    "na": {"rev_pot": 87.39},
+                    "k": {"rev_pot": -80.993},
+                    "ca": {"rev_pot": 137.5},
+                },
+                "mechanisms": {
+                    ("Na", "granule_cell"): {"gnabar": 0.02630163681502},
+                    "Kv3_4": {"gkbar": 0.00237386061632},
+                    "Leak": {"e": -60, "gmax": 9.364092125e-05},
+                    "Ca": {"gcabar": 0.00068197420273},
+                    ("cdp5", "CR"): {},
+                },
+            },
+            "parallel_fiber": {
+                "cable": {"Ra": 100, "cm": 1},
+                "ions": {
+                    "na": {"rev_pot": 87.39},
+                    "k": {"rev_pot": -80.993},
+                    "ca": {"rev_pot": 137.5},
+                },
+                "mechanisms": {
+                    ("Na", "granule_cell"): {"gnabar": 0.01771848449261},
+                    "Kv3_4": {"gkbar": 0.00817568047037},
+                    "Leak": {"e": -60, "gmax": 3.5301616e-07},
+                    "Ca": {"gcabar": 0.0002085683353},
+                    ("cdp5", "CR"): {},
+                },
+            },
+            "axon_initial_segment": {
+                "cable": {"Ra": 100, "cm": 1},
+                "ions": {
+                    "na": {"rev_pot": 87.39},
+                    "k": {"rev_pot": -80.993},
+                    "ca": {"rev_pot": 137.5},
+                },
+                "mechanisms": {
+                    ("Na", "granule_cell_FHF"): {"gnabar": 1.28725006737226},
+                    "Kv3_4": {"gkbar": 0.00649595340654},
+                    "Leak": {"e": -60, "gmax": 0.00029276697557},
+                    "Ca": {"gcabar": 0.00031198539472},
+                    "Km": {"gkbar": 0.00056671971737},
+                    ("cdp5", "CR"): {},
+                },
+            },
+            "axon_hillock": {
+                "cable": {"Ra": 100, "cm": 2},
+                "ions": {
+                    "na": {"rev_pot": 87.39},
+                    "k": {"rev_pot": -80.993},
+                    "ca": {"rev_pot": 137.5},
+                },
+                "mechanisms": {
+                    "Leak": {"e": -60, "gmax": 0.0003695818972},
+                    ("Na", "granule_cell_FHF"): {"gnabar": 0.00928805851462},
+                    "Kv3_4": {"gkbar": 0.02037346310915},
+                    "Ca": {"gcabar": 0.00057726155447},
+                    ("cdp5", "CR"): {},
+                },
+            },
         },
-    }
+    },
+    use_defaults=True,
 )
-schema = bsb_schematic(morpho, dbbs_models.GranuleCellModel)
-cell = get_bpo_cell(schema, constraints)
+
+
+def create_schema(constraints, tags, file_morphology):
+    _m = parse_morphology_file(file_morphology, tags=tags)
+    return bsb_schematic(_m, constraints)
+
+
+schema = create_schema(
+    constraints,
+    {
+        16: ["axon", "axon_hillock"],
+        17: ["axon", "axon_initial_segment"],
+        18: ["axon", "ascending_axon"],
+        19: ["axon", "parallel_fiber"],
+    },
+    r"/home/robin/git/single_cell_test/dbbs_model_test/Granule_cell/morphology/GranuleCell.swc",
+)
+cell = get_bpo_cell(
+    schema,
+    debug=create_schema(
+        dbbs_models.GranuleCellModel,
+        {
+            16: ["axon", "axon_hillock"],
+            17: ["axon", "axon_initial_segment"],
+            18: ["axon", "ascending_axon"],
+            19: ["axon", "parallel_fiber"],
+        },
+        r"/home/robin/git/single_cell_test/dbbs_model_test/Granule_cell/morphology/GranuleCell.swc",
+    ),
+)
 
 soma_loc = ephys.locations.NrnSeclistCompLocation(
     name="soma", seclist_name="soma", sec_index=0, comp_x=0.5
@@ -42,16 +160,16 @@ NUMBER_GENERATIONS = 1  # Maximum number of generations
 
 sweep_protocols = []
 for protocol_name, amplitude in [
-    ("step1", 0.01),
+    # ("step1", 0.01),
     ("step2", 0.016),
-    ("step3", 0.022),
-]:  #
+    # ("step3", 0.022),
+]:
     stim = ephys.stimuli.NrnSquarePulse(
         step_amplitude=amplitude,
         step_delay=100,
-        step_duration=2000,
+        step_duration=100,
         location=soma_loc,
-        total_duration=2200,
+        total_duration=200,
     )
     rec = ephys.recordings.CompRecording(
         name="%s.soma.v" % protocol_name, location=soma_loc, variable="v"
@@ -146,215 +264,6 @@ best_ind = hall_of_fame[0]
 print("Best individual: ", best_ind)
 print("Fitness values: ", best_ind.fitness.values)
 
-grc = neuron_build(schema)
-from patch import p
-
-p.celsius = 32
-p.v_init = -70
-stims = [
-    {"dur": 1000, "delay": 100, "curr": 0.01},
-    {"dur": 1000, "delay": 1200, "curr": 0.016},
-    {"dur": 1000, "delay": 2300, "curr": 0.022},
-]
-
-
-def create_cell(definition, tags, file_morphology):
-    _m = parse_morphology_file(file_morphology, tags=tags)
-    schematic = bsb_schematic(_m, definition)
-    return neuron_build(schematic)
-
-
-def current_injection(*models, dt=0.025, stims=[]):
-    t = p.record(p._ref_t)
-    p.dt = dt
-    for model in models:
-        model.Vm = model.soma[0].record()
-        model._spike_detector = p.NetCon(model.soma[0], None)  # fake connection
-
-        model.injections = []
-        for stim in stims:
-            clamp = model.soma[0].iclamp(
-                x=0.5, delay=stim["delay"], duration=stim["dur"], amplitude=stim["curr"]
-            )
-            model.injections.append(p.record(clamp._ref_i))
-
-        model.spike_times = p.Vector()
-        model._spike_detector.record(model.spike_times)
-
-    p.celsius = 32
-    p.finitialize(-70)
-    p.continuerun(max(*(stim["dur"] + stim["delay"] for stim in stims)) + 100)
-
-    return list(t), [
-        {
-            "name": getattr(model, "name", "Current injection"),
-            "Vm": list(model.Vm),
-            "spks": list(model.spike_times),
-            "Im": sum(np.array(list(im)) for im in model.injections).tolist(),
-        }
-        for model in models
-    ]
-
-
-def adjust_morpho_diam(cell):
-    for i_ses, ses_i in enumerate(cell.sections):
-        if len(ses_i.children()) > 1:
-            for i_chil, chil_i in enumerate(ses_i.children()):
-                if not (chil_i in cell.soma):
-                    chil_i.pt3dchange(0, chil_i.diam3d(1))
-    return cell
-
-
-definitionGrCWT = define_model(
-    {
-        "synapse_types": {
-            "AMPA": {
-                "mechanism": ("AMPA", "granule"),
-                "parameters": {
-                    "tau_facil": 5,
-                    "tau_rec": 8,
-                    "tau_1": 1,
-                    "gmax": 1400,
-                    "U": 0.42,
-                },
-            },
-            "NMDA": {
-                "mechanism": ("NMDA", "granule"),
-                "parameters": {
-                    "tau_facil": 5,
-                    "tau_rec": 8,
-                    "tau_1": 1,
-                    "gmax": 18800,
-                    "U": 0.43,
-                },
-            },
-            "GABA": {"mechanism": ("GABA", "granule"), "parameters": {"U": 0.35}},
-        },
-        "cable_types": {
-            "soma": {
-                "cable": {"Ra": 100, "cm": 2},
-                "ions": {
-                    "k": {"rev_pot": -88},
-                    "ca": {"rev_pot": 137.5},
-                },  # -80.993 / -88
-                "mechanisms": {
-                    "Leak": {"e": -60, "gmax": 0.00029038073716 * 0.455},
-                    "Kv3_4": {"gkbar": 0.00076192450952},
-                    "Kv4_3": {"gkbar": 0.00281496839061},
-                    "Kir2_3": {"gkbar": 0.00074725514702},
-                    "Ca": {"gcabar": 0.00060938071784},
-                    "Kv1_1": {"gbar": 0.00569738264555},
-                    "Kv1_5": {"gKur": 0.00083407556714},
-                    "Kv2_2": {"gKv2_2bar": 1.203410852e-05},
-                    ("cdp5", "CR"): {},
-                },
-            },
-            "dendrites": {
-                "cable": {"Ra": 100, "cm": 2.5},
-                "ions": {"k": {"rev_pot": -88}, "ca": {"rev_pot": 137.5}},
-                "mechanisms": {
-                    "Leak": {"e": -60, "gmax": 0.00025029700737 * 0.455},
-                    ("Leak", "GABA"): {"ggaba": 0.0},
-                    "Ca": {"gcabar": 0.00500128008459},
-                    "Kca1_1": {"gbar": 0.01001807454651},
-                    "Kv1_1": {"gbar": 0.00381819207934},
-                    ("cdp5", "CR"): {},
-                },
-            },
-            "axon": {"cable": {}, "ions": {}, "mechanisms": {}},
-            "ascending_axon": {
-                "cable": {"Ra": 100, "cm": 1},
-                "ions": {
-                    "na": {"rev_pot": 87.39},
-                    "k": {"rev_pot": -88},
-                    "ca": {"rev_pot": 137.5},
-                },
-                "mechanisms": {
-                    ("Na", "granule_cell"): {"gnabar": 0.02630163681502},
-                    "Kv3_4": {"gkbar": 0.00237386061632},
-                    "Leak": {"e": -60, "gmax": 9.364092125e-05},
-                    "Ca": {"gcabar": 0.00068197420273},
-                    ("cdp5", "CR"): {},
-                },
-            },
-            "axon_initial_segment": {
-                "cable": {"Ra": 100, "cm": 1},
-                "ions": {
-                    "na": {"rev_pot": 87.39},
-                    "k": {"rev_pot": -88},
-                    "ca": {"rev_pot": 137.5},
-                },
-                "mechanisms": {
-                    ("Na", "granule_cell_FHF"): {"gnabar": 1.28725006737226},
-                    "Kv3_4": {"gkbar": 0.00649595340654},
-                    "Leak": {"e": -60, "gmax": 0.00029276697557 * 0.455},
-                    "Ca": {"gcabar": 0.00031198539472},
-                    "Km": {"gkbar": 0.00056671971737},
-                    ("cdp5", "CR"): {},
-                },
-            },
-            "axon_hillock": {
-                "cable": {"Ra": 100, "cm": 2},
-                "ions": {
-                    "na": {"rev_pot": 87.39},
-                    "k": {"rev_pot": -88},
-                    "ca": {"rev_pot": 137.5},
-                },
-                "mechanisms": {
-                    "Leak": {"e": -60, "gmax": 0.0003695818972 * 0.455},
-                    ("Na", "granule_cell_FHF"): {"gnabar": 0.00928805851462},
-                    "Kv3_4": {"gkbar": 0.02037346310915},
-                    "Ca": {"gcabar": 0.00057726155447},
-                    ("cdp5", "CR"): {},
-                },
-            },
-            "parallel_fiber": {
-                "cable": {"Ra": 100, "cm": 1},
-                "ions": {
-                    "na": {"rev_pot": 87.39},
-                    "k": {"rev_pot": -88},
-                    "ca": {"rev_pot": 137.5},
-                },
-                "mechanisms": {
-                    ("Na", "granule_cell"): {"gnabar": 0.01771848449261},
-                    "Kv3_4": {"gkbar": 0.00817568047037},
-                    "Leak": {"e": -60, "gmax": 3.5301616e-07},
-                    "Ca": {"gcabar": 0.0002085683353},
-                    ("cdp5", "CR"): {},
-                },
-            },
-        },
-    },
-    use_defaults=True,
-)
-
-tagsGrC = {
-    16: ["axon", "axon_hillock"],
-    17: ["axon", "axon_initial_segment"],
-    18: ["axon", "ascending_axon"],
-    19: ["axon", "parallel_fiber"],
-}
-
-cells = {
-    "models": create_cell(
-        dbbs_models.GranuleCellModel,
-        tagsGrC,
-        r"/home/robin/git/single_cell_test/dbbs_model_test/Granule_cell/morphology/GranuleCell.swc",
-    ),
-    "mfc_adjusted": adjust_morpho_diam(
-        create_cell(
-            definitionGrCWT,
-            tagsGrC,
-            r"/home/robin/git/single_cell_test/dbbs_model_test/Granule_cell/morphology/GranuleCell.swc",
-        )
-    ),
-}
-for name, _c in cells.items():
-    _c.name = name
-
-
-time, res = current_injection(*cells.values(), stims=stims, dt=0.025)
-
 best_ind_dict = cell_evaluator.param_dict(best_ind)
 responses = threestep_protocol.run(cell_model=cell, param_values=best_ind_dict, sim=nrn)
 
@@ -362,10 +271,7 @@ import plotly.graph_objs as go
 
 go.Figure(
     [
-        *[go.Scatter(x=time, y=r["Vm"], name=r["name"]) for r in res],
-        *[
-            go.Scatter(x=resp["time"], y=resp["voltage"], name=name)
-            for name, resp in responses.items()
-        ],
+        go.Scatter(x=resp["time"], y=resp["voltage"], name=name)
+        for name, resp in responses.items()
     ]
 ).write_html("gpop.html")
