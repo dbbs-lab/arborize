8eb8ca7e9b3cacd7060d9a3080e60cf39ec9f44c
Robin De Schepper <robin.deschepper93@gmail.com>
2024-02-19 12:32:46 +0100

    ca marche!

--- temp.py
+++ temp.py
@@ -1,3 +1,5 @@
+import logging
+
 import bluepyopt as bpop
 import bluepyopt.ephys as ephys
 import dbbs_models
@@ -7,119 +9,8 @@ from arborize import define_constraints
 from arborize.optimizers._bluepyopt import get_bpo_cell
 from arborize.schematics import bsb_schematic
 
-constraints = define_constraints(
-    {
-        "synapse_types": {
-            ("AMPA", "granule"): {
-                "tau_facil": 5,
-                "tau_rec": 8,
-                "tau_1": 1,
-                "gmax": 140000,
-                "U": 0.43,
-            },
-            ("NMDA", "granule"): {
-                "tau_facil": 5,
-                "tau_rec": 8,
-                "tau_1": 1,
-                "gmax": 23500,
-                "U": 0.43,
-            },
-            ("GABA", "granule"): {"U": 0.35},
-        },
-        "cable_types": {
-            "soma": {
-                "cable": {"Ra": 100, "cm": 2},
-                "ions": {"k": {"rev_pot": -80.993}, "ca": {"rev_pot": 137.5}},
-                "mechanisms": {
-                    "Leak": {"e": -60, "gmax": [0.00029038073716, 0.00029038073717]},
-                    "Kv3_4": {"gkbar": 0.00076192450952},
-                    "Kv4_3": {"gkbar": 0.00281496839061},
-                    "Kir2_3": {"gkbar": 0.00074725514702},
-                    "Ca": {"gcabar": 0.00060938071784},
-                    "Kv1_1": {"gbar": 0.00569738264555},
-                    "Kv1_5": {"gKur": 0.00083407556714},
-                    "Kv2_2": {"gKv2_2bar": 1.203410852e-05},
-                    ("cdp5", "CR"): {},
-                },
-            },
-            "dendrites": {
-                "cable": {"Ra": 100, "cm": 2.5},
-                "ions": {"k": {"rev_pot": -80.993}, "ca": {"rev_pot": 137.5}},
-                "mechanisms": {
-                    "Leak": {"e": -60, "gmax": 0.00025029700737},
-                    ("Leak", "GABA"): {},
-                    "Ca": {"gcabar": 0.00500128008459},
-                    "Kca1_1": {"gbar": 0.01001807454651},
-                    "Kv1_1": {"gbar": 0.00381819207934},
-                    ("cdp5", "CR"): {},
-                },
-            },
-            "axon": {"cable": {}, "ions": {}, "mechanisms": {}},
-            "ascending_axon": {
-                "cable": {"Ra": 100, "cm": 1},
-                "ions": {
-                    "na": {"rev_pot": 87.39},
-                    "k": {"rev_pot": -80.993},
-                    "ca": {"rev_pot": 137.5},
-                },
-                "mechanisms": {
-                    ("Na", "granule_cell"): {"gnabar": 0.02630163681502},
-                    "Kv3_4": {"gkbar": 0.00237386061632},
-                    "Leak": {"e": -60, "gmax": 9.364092125e-05},
-                    "Ca": {"gcabar": 0.00068197420273},
-                    ("cdp5", "CR"): {},
-                },
-            },
-            "parallel_fiber": {
-                "cable": {"Ra": 100, "cm": 1},
-                "ions": {
-                    "na": {"rev_pot": 87.39},
-                    "k": {"rev_pot": -80.993},
-                    "ca": {"rev_pot": 137.5},
-                },
-                "mechanisms": {
-                    ("Na", "granule_cell"): {"gnabar": 0.01771848449261},
-                    "Kv3_4": {"gkbar": 0.00817568047037},
-                    "Leak": {"e": -60, "gmax": 3.5301616e-07},
-                    "Ca": {"gcabar": 0.0002085683353},
-                    ("cdp5", "CR"): {},
-                },
-            },
-            "axon_initial_segment": {
-                "cable": {"Ra": 100, "cm": 1},
-                "ions": {
-                    "na": {"rev_pot": 87.39},
-                    "k": {"rev_pot": -80.993},
-                    "ca": {"rev_pot": 137.5},
-                },
-                "mechanisms": {
-                    ("Na", "granule_cell_FHF"): {"gnabar": 1.28725006737226},
-                    "Kv3_4": {"gkbar": 0.00649595340654},
-                    "Leak": {"e": -60, "gmax": 0.00029276697557},
-                    "Ca": {"gcabar": 0.00031198539472},
-                    "Km": {"gkbar": 0.00056671971737},
-                    ("cdp5", "CR"): {},
-                },
-            },
-            "axon_hillock": {
-                "cable": {"Ra": 100, "cm": 2},
-                "ions": {
-                    "na": {"rev_pot": 87.39},
-                    "k": {"rev_pot": -80.993},
-                    "ca": {"rev_pot": 137.5},
-                },
-                "mechanisms": {
-                    "Leak": {"e": -60, "gmax": 0.0003695818972},
-                    ("Na", "granule_cell_FHF"): {"gnabar": 0.00928805851462},
-                    "Kv3_4": {"gkbar": 0.02037346310915},
-                    "Ca": {"gcabar": 0.00057726155447},
-                    ("cdp5", "CR"): {},
-                },
-            },
-        },
-    },
-    use_defaults=True,
-)
+
+logging.getLogger().setLevel(logging.DEBUG)
 
 
 def create_schema(constraints, tags, file_morphology):
@@ -128,7 +19,122 @@ def create_schema(constraints, tags, file_morphology):
 
 
 schema = create_schema(
-    constraints,
+    define_constraints(
+        {
+            "synapse_types": {
+                ("AMPA", "granule"): {
+                    "tau_facil": 5,
+                    "tau_rec": 8,
+                    "tau_1": 1,
+                    "gmax": 140000,
+                    "U": 0.43,
+                },
+                ("NMDA", "granule"): {
+                    "tau_facil": 5,
+                    "tau_rec": 8,
+                    "tau_1": 1,
+                    "gmax": 23500,
+                    "U": 0.43,
+                },
+                ("GABA", "granule"): {"U": 0.35},
+            },
+            "cable_types": {
+                "soma": {
+                    "cable": {"Ra": 100, "cm": 2},
+                    "ions": {"k": {"rev_pot": -80.993}, "ca": {"rev_pot": 137.5}},
+                    "mechanisms": {
+                        "Leak": {
+                            "e": -60,
+                            "gmax": [0.00029038073716, 0.00029038073717],
+                        },
+                        "Kv3_4": {"gkbar": 0.00076192450952},
+                        "Kv4_3": {"gkbar": 0.00281496839061},
+                        "Kir2_3": {"gkbar": 0.00074725514702},
+                        "Ca": {"gcabar": 0.00060938071784},
+                        "Kv1_1": {"gbar": 0.00569738264555},
+                        "Kv1_5": {"gKur": 0.00083407556714},
+                        "Kv2_2": {"gKv2_2bar": 1.203410852e-05},
+                        ("cdp5", "CR"): {},
+                    },
+                },
+                "dendrites": {
+                    "cable": {"Ra": 100, "cm": 2.5},
+                    "ions": {"k": {"rev_pot": -80.993}, "ca": {"rev_pot": 137.5}},
+                    "mechanisms": {
+                        "Leak": {"e": -60, "gmax": 0.00025029700737},
+                        ("Leak", "GABA"): {},
+                        "Ca": {"gcabar": 0.00500128008459},
+                        "Kca1_1": {"gbar": 0.01001807454651},
+                        "Kv1_1": {"gbar": 0.00381819207934},
+                        ("cdp5", "CR"): {},
+                    },
+                },
+                "axon": {"cable": {}, "ions": {}, "mechanisms": {}},
+                "ascending_axon": {
+                    "cable": {"Ra": 100, "cm": 1},
+                    "ions": {
+                        "na": {"rev_pot": 87.39},
+                        "k": {"rev_pot": -80.993},
+                        "ca": {"rev_pot": 137.5},
+                    },
+                    "mechanisms": {
+                        ("Na", "granule_cell"): {"gnabar": 0.02630163681502},
+                        "Kv3_4": {"gkbar": 0.00237386061632},
+                        "Leak": {"e": -60, "gmax": 9.364092125e-05},
+                        "Ca": {"gcabar": 0.00068197420273},
+                        ("cdp5", "CR"): {},
+                    },
+                },
+                "parallel_fiber": {
+                    "cable": {"Ra": 100, "cm": 1},
+                    "ions": {
+                        "na": {"rev_pot": 87.39},
+                        "k": {"rev_pot": -80.993},
+                        "ca": {"rev_pot": 137.5},
+                    },
+                    "mechanisms": {
+                        ("Na", "granule_cell"): {"gnabar": 0.01771848449261},
+                        "Kv3_4": {"gkbar": 0.00817568047037},
+                        "Leak": {"e": -60, "gmax": 3.5301616e-07},
+                        "Ca": {"gcabar": 0.0002085683353},
+                        ("cdp5", "CR"): {},
+                    },
+                },
+                "axon_initial_segment": {
+                    "cable": {"Ra": 100, "cm": 1},
+                    "ions": {
+                        "na": {"rev_pot": 87.39},
+                        "k": {"rev_pot": -80.993},
+                        "ca": {"rev_pot": 137.5},
+                    },
+                    "mechanisms": {
+                        ("Na", "granule_cell_FHF"): {"gnabar": 1.28725006737226},
+                        "Kv3_4": {"gkbar": 0.00649595340654},
+                        "Leak": {"e": -60, "gmax": 0.00029276697557},
+                        "Ca": {"gcabar": 0.00031198539472},
+                        "Km": {"gkbar": 0.00056671971737},
+                        ("cdp5", "CR"): {},
+                    },
+                },
+                "axon_hillock": {
+                    "cable": {"Ra": 100, "cm": 2},
+                    "ions": {
+                        "na": {"rev_pot": 87.39},
+                        "k": {"rev_pot": -80.993},
+                        "ca": {"rev_pot": 137.5},
+                    },
+                    "mechanisms": {
+                        "Leak": {"e": -60, "gmax": 0.0003695818972},
+                        ("Na", "granule_cell_FHF"): {"gnabar": 0.00928805851462},
+                        "Kv3_4": {"gkbar": 0.02037346310915},
+                        "Ca": {"gcabar": 0.00057726155447},
+                        ("cdp5", "CR"): {},
+                    },
+                },
+            },
+        },
+        use_defaults=True,
+    ),
     {
         16: ["axon", "axon_hillock"],
         17: ["axon", "axon_initial_segment"],
@@ -257,15 +263,21 @@ optimisation = bpop.optimisations.DEAPOptimisation(
     seed=333,
 )
 
-final_pop, hall_of_fame, logs, hist = optimisation.run(max_ngen=NUMBER_GENERATIONS)
+# final_pop, hall_of_fame, logs, hist = optimisation.run(max_ngen=NUMBER_GENERATIONS)
 
 # best individual
-best_ind = hall_of_fame[0]
-print("Best individual: ", best_ind)
-print("Fitness values: ", best_ind.fitness.values)
-
-best_ind_dict = cell_evaluator.param_dict(best_ind)
-responses = threestep_protocol.run(cell_model=cell, param_values=best_ind_dict, sim=nrn)
+# best_ind = hall_of_fame[0]
+# print("Best individual: ", best_ind)
+# print("Fitness values: ", best_ind.fitness.values)
+#
+# best_ind_dict = cell_evaluator.param_dict(best_ind)
+# print("Ind dict", best_ind_dict)
+responses = threestep_protocol.run(
+    cell_model=cell,
+    param_values={"gmax_glia__dbbs__Leak__0_soma": 0.00029038073716554835},
+    sim=nrn,
+    isolate=False,
+)
 
 import plotly.graph_objs as go
 
